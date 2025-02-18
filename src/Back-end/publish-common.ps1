5e5540b6cb51432649b04cafb2f705bf20d99e3d
PH-Lars <larsvdbosch1994@gmail.com>
2025-02-18 16:44:31 +0100

    added .net packaging script

---
+++ src/Back-end/publish-common.ps1
@@ -0,0 +1,37 @@
+# Instellingen voor versienummering
+param (
+    [string]$Major = "1",
+    [string]$Minor = "0",
+    [string]$Patch = "0"
+)
+
+# Genereer het timestamp-gedeelte (YYYYMMDD-HHMM)
+$timestamp = Get-Date -Format "yyyyMMdd-HHmm"
+
+# Bouw de complete versie string
+$Version = "$Major.$Minor.$Patch-ci-$timestamp"
+
+Write-Host "Building package with version: $Version"
+
+# Voer `dotnet pack` uit met de juiste versie
+dotnet pack .\Libraries\NewHeap.Platform.Common -c Release /p:Version=$Version
+dotnet pack .\Libraries\NewHeap.Platform.AspNet.Common -c Release /p:Version=$Version
+
+# Controleer of het packen is geslaagd
+if ($LASTEXITCODE -ne 0) {
+    Write-Host "Packing failed!"
+    exit 1
+}
+
+Write-Host "Package built successfully: Version $Version"
+
+dotnet nuget push --source "https://pkgs.dev.azure.com/NewHeap/NewHeap-Platform/_packaging/NewHeap-Platform/nuget/v3/index.json" --api-key az ./Libraries/NewHeap.Platform.Common/bin/Release/NewHeap.Platform.Common.$Version.nupkg
+
+dotnet nuget push --source "https://pkgs.dev.azure.com/NewHeap/NewHeap-Platform/_packaging/NewHeap-Platform/nuget/v3/index.json" --api-key az ./Libraries/NewHeap.Platform.AspNet.Common/bin/Release/NewHeap.Platform.AspNet.Common.$Version.nupkg
+
+if ($LASTEXITCODE -ne 0) {
+    Write-Host "Publishing failed!"
+    exit 1
+}
+
+Write-Host "Published successfully: Version $Version"
