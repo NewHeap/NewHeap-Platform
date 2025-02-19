3c856f586313d726a3bd9aea1536b118e9668605
PH-Lars <larsvdbosch1994@gmail.com>
2025-02-19 17:19:31 +0100

    Authenticated by default

--- src/Back-end/Libraries/NewHeap.Platform.AspNet.Common/Controllers/BaseController.cs
+++ src/Back-end/Libraries/NewHeap.Platform.AspNet.Common/Controllers/BaseController.cs
@@ -1,4 +1,6 @@
 ﻿using AutoMapper;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
@@ -24,6 +26,7 @@ using System.Text;
 namespace NewHeap.Platform.AspNet.Common.Controllers;
 
 [ApiController]
+[Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
 public abstract partial class BaseController<TController, TBaseEntity> : ControllerBase
     where TController : BaseController<TController, TBaseEntity>
     where TBaseEntity : class
