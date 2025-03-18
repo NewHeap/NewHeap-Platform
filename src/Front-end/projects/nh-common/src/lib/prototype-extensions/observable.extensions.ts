ed2c0ee9073b607f6d6423123c29a0f6fd2e5edb
PH-Lars <larsvdbosch1994@gmail.com>
2025-03-18 15:57:53 +0100

    taskresult lastvaluefrom

--- src/Front-end/projects/nh-common/src/lib/prototype-extensions/observable.extensions.ts
+++ src/Front-end/projects/nh-common/src/lib/prototype-extensions/observable.extensions.ts
@@ -1,11 +1,27 @@
 ﻿import {lastValueFrom, Observable} from 'rxjs';
+import {TaskResult} from "../models/misc.models";
+import {NhApiUtil} from "../util/nh-api-util";
 
 declare module "rxjs/internal/Observable" {
   interface Observable<T> {
     lastValueFrom(): Promise<T>;
+    taskResultLastValueFrom(): Promise<TaskResult<T>>;
   }
 }
 
 Observable.prototype.lastValueFrom = function () {
   return lastValueFrom(this);
 }
+
+Observable.prototype.taskResultLastValueFrom = async function <T>(): Promise<TaskResult<T>> {
+  const taskResult = new TaskResult<T>();
+  try {
+    taskResult.data = await lastValueFrom(this);
+  } catch (ex) {
+    const errResult = NhApiUtil.taskResultFromResponse(ex);
+    errResult.copyTo(taskResult);
+  }
+
+  return taskResult;
+}
+
