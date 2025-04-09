d705a04686c97042cc7b95991bd04f112d48fa93
PH-Lars <larsvdbosch1994@gmail.com>
2025-04-09 17:39:31 +0200

    OneOf addits

---
+++ src/Back-end/Libraries/NewHeap.Platform.AspNet.Common/Converters/OneOfJsonConverter.cs
@@ -0,0 +1,27 @@
+﻿using Newtonsoft.Json;
+using OneOf;
+using System.Reflection;
+
+namespace NewHeap.Platform.AspNet.Common.Converters;
+
+public class OneOfJsonConverter : JsonConverter
+{
+    public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
+    {
+        if (value is IOneOf)
+        {
+            value = ((IOneOf)value).Value;
+        }
+        serializer.Serialize(writer, value);
+    }
+
+    public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
+    {
+        throw new NotImplementedException();
+    }
+
+    public override bool CanConvert(Type objectType)
+    {
+        return objectType.GetTypeInfo().ImplementedInterfaces.Contains(typeof(IOneOf));
+    }
+}
---
+++ src/Back-end/Libraries/NewHeap.Platform.AspNet.Common/SchemaFilters/OneOfSchemaTransformer.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.OpenApi;
+using Microsoft.OpenApi.Any;
+using Microsoft.OpenApi.Models;
+
+namespace NewHeap.Platform.AspNet.Common.SchemaFilters;
+public class OneOfSchemaTransformer : IOpenApiSchemaTransformer
+{
+    public Task TransformAsync(
+        OpenApiSchema schema,
+        OpenApiSchemaTransformerContext context,
+        CancellationToken cancellationToken
+        )
+    {   
+        // TODO: testing inopg
+        return Task.CompletedTask;
+    }
+}
