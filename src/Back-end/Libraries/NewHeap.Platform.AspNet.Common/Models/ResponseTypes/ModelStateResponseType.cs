4018e63922cfa0fe771be87369301f0438908e95
PH-Lars <larsvdbosch1994@gmail.com>
2025-04-14 13:06:45 +0200

    Responsetype

---
+++ src/Back-end/Libraries/NewHeap.Platform.AspNet.Common/Models/ResponseTypes/ModelStateResponseType.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NewHeap.Platform.AspNet.Common.Models.ResponseTypes;
+public class ModelStateResponseType : Dictionary<string, string[]>
+{
+}
