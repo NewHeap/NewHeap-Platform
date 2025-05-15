14253651b1f65c86ce52bc49e7e8cefb8a4f9157
PH-Lars <larsvdbosch1994@gmail.com>
2025-05-15 13:27:05 +0200

    Fix

--- src/Back-end/Libraries/NewHeap.Platform.Common/Extensions/CollectionRequestModelExtensions.cs
+++ src/Back-end/Libraries/NewHeap.Platform.Common/Extensions/CollectionRequestModelExtensions.cs
@@ -10,9 +10,32 @@ using System.Threading.Tasks;
 namespace NewHeap.Platform.Common.Extensions;
 public static class CollectionRequestModelExtensions
 {
-    public static IEnumerable<T> PageSkipTake<T>(this IEnumerable<T> q, int page, int itemsPerPage)
+    public static List<T> PageSkipTake<T>(this List<T> q, int page, int itemsPerPage)
     {
         if(page < 1)
+        {
+            page = 1;
+        }
+
+        if (itemsPerPage < 0)
+        {
+            itemsPerPage = 0;
+        }
+
+        return q
+            .Skip((page - 1) * itemsPerPage)
+            .Take(itemsPerPage)
+            .ToList();
+    }
+
+    public static List<T> PageSkipTake<T>(this List<T> q, IBaseCollectionRequestModel requestModel)
+    {
+        return q.PageSkipTake(requestModel.Page, requestModel.ItemsPerPage);
+    }
+
+    public static IQueryable<T> PageSkipTake<T>(this IQueryable<T> q, int page, int itemsPerPage)
+    {
+        if (page < 1)
         {
             page = 1;
         }
@@ -27,7 +50,7 @@ public static class CollectionRequestModelExtensions
             .Take(itemsPerPage);
     }
 
-    public static IEnumerable<T> PageSkipTake<T>(this IEnumerable<T> q, IBaseCollectionRequestModel requestModel)
+    public static IQueryable<T> PageSkipTake<T>(this IQueryable<T> q, IBaseCollectionRequestModel requestModel)
     {
         return q.PageSkipTake(requestModel.Page, requestModel.ItemsPerPage);
     }
