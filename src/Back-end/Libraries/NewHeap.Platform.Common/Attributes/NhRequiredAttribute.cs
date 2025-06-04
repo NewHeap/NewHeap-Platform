6c13f39b71003ea32ce73e6de67a3345662f0a4b
PH-Lars <larsvdbosch1994@gmail.com>
2025-06-04 12:53:57 +0200

    NhRequired DisallowAllDefaultValues

--- src/Back-end/Libraries/NewHeap.Platform.Common/Attributes/NhRequiredAttribute.cs
+++ src/Back-end/Libraries/NewHeap.Platform.Common/Attributes/NhRequiredAttribute.cs
@@ -2,17 +2,35 @@
 using NewHeap.Platform.Common.Translations;
 using System.ComponentModel.DataAnnotations;
 
-namespace NewHeap.Platform.AspNet.Common.Attributes;
+namespace NewHeap.Platform.Common.Attributes;
 
 [AttributeUsage(AttributeTargets.Property | AttributeTargets.Field | AttributeTargets.Parameter)]
 public partial class NhRequiredAttribute : RequiredAttribute
 {
+    public bool DisallowAllDefaultValues { get; set; }
+
     protected override ValidationResult? IsValid(object? value, ValidationContext validationContext)
     {
         var stringLocalizer =
             (validationContext.GetService(typeof(IStringLocalizer<SharedDataAnnotationRecources>)) as
                 IStringLocalizer<SharedDataAnnotationRecources>)!;
         ErrorMessage = stringLocalizer["The {0} field is required."]?.Value ?? "The {0} field is required.";
+
+        if (DisallowAllDefaultValues && value is not null)
+        {
+            Type type = value.GetType();
+
+            if (type.IsValueType)
+            {
+                object? defaultValue = Activator.CreateInstance(type);
+
+                if (value.Equals(defaultValue))
+                {
+                    string error = FormatErrorMessage(validationContext.DisplayName);
+                    return new ValidationResult(error);
+                }
+            }
+        }
 
         return base.IsValid(value, validationContext);
     }
