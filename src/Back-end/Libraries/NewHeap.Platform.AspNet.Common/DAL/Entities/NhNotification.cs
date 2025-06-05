4bcc340d19d04acd99f6ed13c288a9ac2988f45c
PH-Lars <larsvdbosch1994@gmail.com>
2025-06-05 16:51:23 +0200

    Init

---
+++ src/Back-end/Libraries/NewHeap.Platform.AspNet.Common/DAL/Entities/NhNotification.cs
@@ -0,0 +1,57 @@
+﻿using NewHeap.Platform.AspNet.Common.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NewHeap.Platform.AspNet.Common.DAL.Entities;
+
+public enum NhNotificationPriority
+{
+    Unknown = 0,
+    Low = 10,
+    Normal = 20,
+    High = 30,
+    Critical = 40
+}
+
+public enum NhNotificationStatus
+{
+    // Job persisted and registered in the storage
+    Queued = 0,
+
+    //Job dispatched to background job framework
+    Scheduled = 10,
+
+    //Framework is executing the job
+    Processing = 20,
+
+    //Result is here, it failed
+    Failed = 30,
+
+    //Result is here, success
+    Succeeded = 40
+}
+
+public partial class NhNotification : IdDbEntity
+{
+    [Key]
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+    public Guid Id { get; set; }
+    public DateTimeOffset CreationDateTime { get; set; } = DateTimeOffset.UtcNow;
+    public DateTimeOffset LastModifiedDateTime { get; set; } = DateTimeOffset.UtcNow;
+
+    public Guid? CreatedByUserId { get; set; }
+
+    [Column(TypeName = "smallint")]
+    public NhNotificationPriority Priority { get; set; } = NhNotificationPriority.Normal;
+
+    [StringLength(256)]
+    public string Title { get; set; } = string.Empty;
+
+    [StringLength(5)]
+    public string LanguageCulture { get; set; } = string.Empty;
+}
---
+++ src/Back-end/Libraries/NewHeap.Platform.AspNet.Common/DAL/Entities/NhNotificationDelivery.cs
@@ -0,0 +1,21 @@
+﻿using NewHeap.Platform.AspNet.Common.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NewHeap.Platform.AspNet.Common.DAL.Entities;
+
+public partial class NhNotificationDelivery : IdDbEntity
+{
+    [Key]
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+    public Guid Id { get; set; }
+    public DateTimeOffset CreationDateTime { get; set; } = DateTimeOffset.UtcNow;
+    public DateTimeOffset LastModifiedDateTime { get; set; } = DateTimeOffset.UtcNow;
+    public Guid NotificationId { get; set; }
+
+}
