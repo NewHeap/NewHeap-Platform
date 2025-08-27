0d83edca3e1163261f89f91594710606cb7cc218
PH-Lars <larsvdbosch1994@gmail.com>
2025-08-27 14:00:52 +0200

    cleanup

--- src/Front-end/projects/nh-common/src/lib/pipes/date.pipes.ts
+++ src/Front-end/projects/nh-common/src/lib/pipes/date.pipes.ts
@@ -1,6 +1,5 @@
 import { DatePipe } from '@angular/common';
 import { Pipe, PipeTransform } from '@angular/core';
-import { TranslateService } from '@ngx-translate/core';
 import {NhCommonModuleConfig} from "../models/config.models";
 
 @Pipe({
