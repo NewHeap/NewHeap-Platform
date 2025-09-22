fb29ade79ff4df8e55be876fd445a3f79d592deb
PH-Lars <larsvdbosch1994@gmail.com>
2025-09-22 14:07:39 +0200

    fix

--- src/Back-end/Libraries/NewHeap.Platform.AspNet.Common/Models/Mutate/UserMutateModels.cs
+++ src/Back-end/Libraries/NewHeap.Platform.AspNet.Common/Models/Mutate/UserMutateModels.cs
@@ -52,6 +52,10 @@ public class NhResetPasswordUserMutateModel
     [NhRequired]
     public string? Token { get; set; }
 
+
+    [NhRequired]
+    public Guid? UserId { get; set; }
+
     [NhRequired]
     [StringLength(100, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 6)]
     [DataType(DataType.Password)]
