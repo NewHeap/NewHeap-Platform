65b2dcdcd1d791746ddf4a7f9b0fd62f2986eca4
Daniel Thomassen <daniel@newheap.com>
2025-12-10 11:12:04 +0100

    Improved generated column names to be escaped

--- src/Back-end/Libraries/NewHeap.Platform.Common/Extensions/IModelExtensions.cs
+++ src/Back-end/Libraries/NewHeap.Platform.Common/Extensions/IModelExtensions.cs
@@ -9,37 +9,39 @@ public static class IModelExtensions
     public static string Column<T>(this IModel model, Expression<Func<T, object?>> prop, bool prefixTable = true)
     {
         var entityDefinition = model.FindEntityType(typeof(T));
-        
+
         if (entityDefinition == null)
         {
             throw new InvalidOperationException("Entity type not found in model.");
         }
-        
-        var memberExpression = prop.Body as MemberExpression 
+
+        var memberExpression = prop.Body as MemberExpression
                                ?? (prop.Body as UnaryExpression)?.Operand as MemberExpression;
 
         if (memberExpression == null)
         {
             throw new InvalidOperationException($"Expression {prop.Body} does not refer to a property.");
         }
-        
+
         var memberName = memberExpression.Member.Name;
         if (memberName == null)
         {
-            throw new InvalidOperationException($"Property {memberExpression.Member.Name} not found in entity type {typeof(T).Name}");
+            throw new InvalidOperationException(
+                $"Property {memberExpression.Member.Name} not found in entity type {typeof(T).Name}");
         }
-        
+
         var column = entityDefinition.GetProperty(memberName).GetColumnName();
 
         if (!prefixTable)
         {
             return $"[{column}]";
         }
-        
-        var tableName = entityDefinition.GetSchemaQualifiedTableName();
+
+        var tableName = string.Join('.',
+            entityDefinition.GetSchemaQualifiedTableName()!.Split('.').Select(x => $"[{x}]"));
         return $"{tableName}.[{column}]";
     }
-    
+
     public static string Table<T>(this IModel model)
     {
         var entityDefinition = model.FindEntityType(typeof(T));
@@ -48,7 +50,13 @@ public static class IModelExtensions
         {
             throw new InvalidOperationException("Entity type not found in model.");
         }
-        
-        return entityDefinition.GetSchemaQualifiedTableName() ?? throw new InvalidOperationException($"Entity type {typeof(T).Name} does not refer to a table.");
+
+        var table = entityDefinition.GetSchemaQualifiedTableName();
+        if (table == null)
+        {
+            throw new InvalidOperationException($"Entity type {typeof(T).Name} does not refer to a table.");
+        }
+
+        return string.Join('.', table!.Split('.').Select(x => $"[x]"));
     }
 }
